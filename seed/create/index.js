53fe47120faf1bd170fb508b8336ddc28b7d4969
Simon MacDonald <simon.macdonald@gmail.com>
2021-07-26 17:27:11 -0400

    🌰  Add seed data

---
+++ seed/create/index.js
@@ -0,0 +1,15 @@
+let arc = require("@architect/functions");
+let data = require("@begin/data");
+
+let players = require("./players");
+let games = require("./games");
+
+exports.handler = async function create(req) {
+  games = games.map((game) => {
+    return { players, ...game };
+  });
+  await data.set(games);
+  return {
+    statusCode: 200,
+  };
+};
